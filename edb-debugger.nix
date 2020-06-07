ea8f04373bd372f52c0307462adf8319f8b5fbf1
main <main@user>
2020-06-07 14:15:50 -0700

    stuff

--- edb-debugger.nix
+++ edb-debugger.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation {
   src = pkgs.fetchFromGitHub {
     owner = "canselcik";
     repo = "edb-debugger";
-    rev = "c04556fd887b613005a866442adbfe780211b8b5";
-    sha256 = "0a892mms1lbj556yk7qk5ddi5hhg5z4awbciva9w2g70kh6274w3";
+    rev = "29ce1805352cd96125d812bfefea6560dc8e44da";
+    sha256 = "0scks2cq965px2s8h27w78vyh519yyh5djzj42rda1rafmzps91d";
     fetchSubmodules = true;
   };
   buildInputs = [ pkgconfig cmake boost capstone qt512.full double-conversion libGL graphviz ];
