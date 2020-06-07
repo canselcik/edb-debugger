c04556fd887b613005a866442adbfe780211b8b5
main <main@user>
2020-06-06 17:31:15 -0700

    stuff

--- edb-debugger.nix
+++ edb-debugger.nix
@@ -2,9 +2,12 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
   name = "edb-debugger";
-  src = nixpkgs.fetchGit {
-    url = "git@github.com:canselcik/edb-debugger.git";
-    ref = "master";
+  src = pkgs.fetchFromGitHub {
+    owner = "canselcik";
+    repo = "edb-debugger";
+    rev = "f552269818e5b4eb906cea73e8f15691c7866cd9";
+    sha256 = "0a892mms1lbj556yk7qk5ddi5hhg5z4awbciva9w2g70kh6274w3";
   };
+
   buildInputs = [ pkgconfig cmake boost capstone qt512.full double-conversion libGL graphviz ];
 }
